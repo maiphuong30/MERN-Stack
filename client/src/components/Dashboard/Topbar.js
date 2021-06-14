458d1dd14728c562568b518c76e0cd6cac316bf7
maiphuong30 <maiphuong30.12.2000@gmail.com>
2021-06-14 12:53:27 +0700

    xoa bot code thua

--- client/src/components/Dashboard/Topbar.js
+++ client/src/components/Dashboard/Topbar.js
@@ -27,46 +27,6 @@ class Topbar extends Component {
                         <i class="fas fa-bell fa-fw"></i>
                         <span class="badge badge-danger badge-counter">3+</span>
                     </a>
-                    <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                        aria-labelledby="alertsDropdown">
-                        <h6 class="dropdown-header">
-                            Alerts Center
-                        </h6>
-                        <a class="dropdown-item d-flex align-items-center" href="#">
-                            <div class="mr-3">
-                                <div class="icon-circle bg-primary">
-                                    <i class="fas fa-file-alt text-white"></i>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="small text-gray-500">December 12, 2019</div>
-                                <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                            </div>
-                        </a>
-                        <a class="dropdown-item d-flex align-items-center" href="#">
-                            <div class="mr-3">
-                                <div class="icon-circle bg-success">
-                                    <i class="fas fa-donate text-white"></i>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="small text-gray-500">December 7, 2019</div>
-                                $290.29 has been deposited into your account!
-                            </div>
-                        </a>
-                        <a class="dropdown-item d-flex align-items-center" href="#">
-                            <div class="mr-3">
-                                <div class="icon-circle bg-warning">
-                                    <i class="fas fa-exclamation-triangle text-white"></i>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="small text-gray-500">December 2, 2019</div>
-                                Spending Alert: We've noticed unusually high spending for your account.
-                            </div>
-                        </a>
-                        <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-                    </div>
                 </li>
                 <li class="nav-item dropdown no-arrow mx-1">
                     <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
@@ -79,7 +39,7 @@ class Topbar extends Component {
                 <div class="topbar-divider d-none d-sm-block"></div>
 
                 <li class="nav-item dropdown no-arrow">
-                    <a class="nav-link dropdown-toggle" href="#">
+                    <a class="nav-link" href="#">
                         <span class="mr-2 d-none d-lg-inline text-gray-600 small"><button class="btn btn-sm btn-outline-secondary" type="submit" name="nut" id="submit">Đăng xuất</button></span>
                     </a>
                 </li>
